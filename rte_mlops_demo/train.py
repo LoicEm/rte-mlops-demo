68e42ca6f7a9164b75fa1edb79b1e1e0dfb0c893
Loic Macherel <loic.macherel@gmail.com>
2022-01-29 15:27:37 +0100

    Run experiment with an ARIMA with easy parameters

--- rte_mlops_demo/train.py
+++ rte_mlops_demo/train.py
@@ -8,7 +8,7 @@ import pandas as pd
 import sktime
 
 from sktime.forecasting.model_selection import temporal_train_test_split
-from sktime.forecasting.arima import AutoARIMA
+from sktime.forecasting.arima import ARIMA
 
 from rte_mlops_demo.metrics import fc_mape
 
@@ -36,12 +36,9 @@ if __name__ == "__main__":
     )
     y = df.set_index("datetime")["total_consumption"]
 
-    # Predict for 24 hours (48 30-minute periods)
-    forecast_horizon = np.arange(1, 49)
-
     # Create a test set of 7 days
     y_train, y_test = temporal_train_test_split(y, test_size=2 * 24 * 7)
-    forecaster = AutoARIMA()
+    forecaster = ARIMA(order=(2, 1, 1), n_jobs=-1)
     forecaster.fit(y_train)
     y_pred = forecaster.predict(np.arange(1, y_test.size + 1))
 
